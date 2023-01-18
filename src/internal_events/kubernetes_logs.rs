a4a7f6fd2dfbc0f068fbaf77bb97d52c75a0bbfe
Mathew Heard <splitice@users.noreply.github.com>
2023-01-18 11:26:11 +1100

    remove pod metrics

--- src/internal_events/kubernetes_logs.rs
+++ src/internal_events/kubernetes_logs.rs
@@ -28,24 +28,13 @@ impl InternalEvent for KubernetesLogsEventsReceived<'_> {
             byte_size = %self.byte_size,
             file = %self.file,
         );
-        match self.pod_info {
-            Some(pod_info) => {
-                let pod_name = pod_info.name;
-                let pod_namespace = pod_info.namespace;
-
-                counter!("component_received_events_total", 1, "pod_name" => pod_name.clone(), "pod_namespace" => pod_namespace.clone());
-                counter!("component_received_event_bytes_total", self.byte_size as u64, "pod_name" => pod_name.clone(), "pod_namespace" => pod_namespace.clone());
-                counter!("events_in_total", 1, "pod_name" => pod_name, "pod_namespace" => pod_namespace);
-            }
-            None => {
-                counter!("component_received_events_total", 1);
-                counter!(
-                    "component_received_event_bytes_total",
-                    self.byte_size as u64
-                );
-                counter!("events_in_total", 1);
-            }
-        }
+        
+        counter!("component_received_events_total", 1);
+        counter!(
+            "component_received_event_bytes_total",
+            self.byte_size as u64
+        );
+        counter!("events_in_total", 1);
     }
 }
 
